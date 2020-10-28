fce6ca23d75150463a020279f6947adc95dae9a3
Arthur Guédon <arthur.guedon@student-cs.fr>
2020-10-28 18:29:14 +0100

    adds icon

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -5,7 +5,12 @@ import SearchField from './SearchField'
 
 const Header =  ({name})=>(
     <div className="header__container">
-        <h1 className="header__title">Spotify App</h1>
+        
+        <div>
+
+            <img src="https://www.flaticon.com/svg/static/icons/svg/725/725281.svg"/>
+            <h1 className="header__title">Spotify App</h1>
+        </div>
         <SearchField/>
         <h1 className="header__title">{name &&(<span>Bienvenue {name}</span>)}</h1>
     </div>
